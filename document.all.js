4236a00fa0f28fffb492f990e1c9937d2c2861c0
yuchangqing <2466857975@qq.com>
2022-07-19 15:17:18 +0800

    js env

--- document.all.js
+++ document.all.js
@@ -3,7 +3,9 @@ const vm = require('vm');
 v8.setFlagsFromString('--allow-natives-syntax');
 let undetectable = vm.runInThisContext('%GetUndetectable()');
 v8.setFlagsFromString('--no-allow-natives-syntax');
-function HTMLAllCollection(){}
+function HTMLAllCollection(){
+    return undetectable
+}
 Object.defineProperties(HTMLAllCollection.prototype, {
     [Symbol.toStringTag]: {
         value: 'HTMLAllCollection',
@@ -13,5 +15,14 @@ Object.defineProperties(HTMLAllCollection.prototype, {
 undetectable.__proto__ = HTMLAllCollection.prototype
 console.log(typeof undetectable, !!undetectable, undetectable + '')
 console.log(undetectable)
+document = {
+    all: new HTMLAllCollection()
+}
+document.all[0] = 111
+
+console.log(document.all)
+console.log(document.all(0))
+console.log(document.all[0])
+console.log(typeof document.all, !!document.all, document.all + '')
 
 
